799f7fe60562af731438cddfb63c877075fbc6ca
Florencia Camona <fecamona@hotmail.com>
2023-09-22 12:59:00 -0300

    se sube la version 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const IMAGES_DATA = {
     categorias: {
         sinreserva: [
@@ -18,7 +17,8 @@ const IMAGES_DATA = {
     leyendas: [
         "7-45",
         "10-46",
-        "Adulta-44"
+        "Adulta-44", 
+        "ConReservas-43"
     ],
     descriptores: [
         "ComplejidadTematicas", 
@@ -34,117 +34,22 @@ const IMAGES_DATA = {
     ddDataConReserva: [],
     ddDataLeyendas: []
 }
-
 let selectCatSinReserva = $("#select-categoria-sinreserva");
 let selectCatConReserva = $("#select-categoria-conreserva");
 let selectLeyenda = $("#select-leyenda");
 let checkboxDescriptores = $("#checkbox-descriptores");
-
-function cargarSelectCategorias(){
-
-    let categorias = IMAGES_DATA.categorias;
-
+let categorias = IMAGES_DATA.categorias;
+function cargarSelectCategoriasSinreserva(){
+    selectCatSinReserva.innerHTML = "";
     for (const cat of categorias.sinreserva) {
-
         let imgUrl = "assets/calificacion-edad/Caec_" + cat + "Trans.png";
-
         selectCatSinReserva.innerHTML += `<option></option>`;
-
         IMAGES_DATA.ddDataSinReserva.push({
             imageSrc: imgUrl,
             value: imgUrl,
             selected: false
         })
     }
-
-    for (const cat of categorias.conreserva) {
-
-        let imgUrl = "assets/calificacion-edad/Caec_" + cat + "Trans.png";
-
-        selectCatConReserva.innerHTML += `<option></option>`;
-
-        IMAGES_DATA.ddDataConReserva.push({
-            imageSrc: imgUrl,
-            value: imgUrl,
-            selected: false
-        })
-    }
-
-}
-
-
-function cargarSelectLeyendas(){
-
-    let leyendas = IMAGES_DATA.leyendas;
-
-    for (const leyenda of leyendas) {
-
-        let imgUrl = "assets/leyendas/Caec_Leyenda-" + leyenda + ".png";
-
-        selectLeyenda.innerHTML += `<option></option>`;
-
-        IMAGES_DATA.ddDataLeyendas.push({
-            imageSrc: imgUrl,
-            value: imgUrl,
-            selected: false
-        })
-    }
-
-}
-function cargarCheckboxDescriptores() {
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    var imagenContainer = document.getElementById("imagenContainer");
-    var imagenesSeleccionadas = [];
-  
-    checkboxes.forEach(function (checkbox) {
-      checkbox.addEventListener("change", function () {
-        var descriptor = checkbox.getAttribute("name");
-  
-        if (checkbox.checked) {
-          var imagen = document.createElement("img");
-          imagen.src = `./assets/descriptores-tematicos/descriporestrans/${descriptor}.png`;
-          imagen.width = 330;
-          imagenesSeleccionadas.push({ checkbox, imagen }); // Agregar checkbox e imagen al array
-        } else {
-          // Filtrar el array para eliminar el elemento correspondiente
-          imagenesSeleccionadas = imagenesSeleccionadas.filter(function (item) {
-            return item.checkbox !== checkbox;
-          });
-        }
-  
-        // Actualizar el contenido del contenedor de imágenes
-        imagenContainer.innerHTML = "";
-        imagenesSeleccionadas.forEach(function (item) {
-          imagenContainer.appendChild(item.imagen);
-        });
-      });
-    });
-  }
-  
-
-
-
-function descargarLogo(){
-    html2canvas(document.querySelector("#logo")).then((canvas) => {
-        let a = document.createElement('a');
-        document.body.appendChild(a)
-        a.href = canvas.toDataURL("image/png", 1.0);
-        a.download = 'logo.png';
-        a.click();
-        
-    })
-}
-
-
-
-document.addEventListener("DOMContentLoaded", function(){
- 
-    cargarSelectCategorias()
-
-    cargarSelectLeyendas()
-
-    cargarCheckboxDescriptores();
-
     selectCatSinReserva.ddslick({
         data: IMAGES_DATA.ddDataSinReserva,
         selectText: "Categoría sin reservas",
@@ -154,7 +59,18 @@ document.addEventListener("DOMContentLoaded", function(){
             `
         }   
     });
-
+}
+function cargarSelectCategoriasConreserva(){
+    selectCatConReserva.innerHTML = "";
+    for (const cat of categorias.conreserva) {
+        let imgUrl = "assets/calificacion-edad/Caec_" + cat + "Trans.png";
+        selectCatConReserva.innerHTML += `<option></option>`;
+        IMAGES_DATA.ddDataConReserva.push({
+            imageSrc: imgUrl,
+            value: imgUrl,
+            selected: false
+        })
+    }
     selectCatConReserva.ddslick({
         data: IMAGES_DATA.ddDataConReserva,
         selectText: "Categoría con reservas",
@@ -164,10 +80,22 @@ document.addEventListener("DOMContentLoaded", function(){
             `
         }   
     });
-
+}
+function cargarSelectLeyendas(){
+    let leyendas = IMAGES_DATA.leyendas;
+    for (const leyenda of leyendas) {
+        let imgUrl = "assets/leyendas/Caec_Leyenda-" + leyenda + ".png";
+        selectLeyenda.innerHTML += `<option></option>`;
+        IMAGES_DATA.ddDataLeyendas.push({
+            imageSrc: imgUrl,
+            value: imgUrl,
+            selected: false
+        })
+    }
     selectLeyenda.ddslick({
         data: IMAGES_DATA.ddDataLeyendas,
         selectText: "Leyenda",
+        width: 350,
         onSelected: function(data){
             if(data.selectedData.value=="sinleyenda"){
                 document.getElementById("logo-leyenda").innerHTML = "";
@@ -178,15 +106,69 @@ document.addEventListener("DOMContentLoaded", function(){
                     <div><img class="logo-leyenda" src="${data.selectedData.value}"></div>
                 `
             }
-      
         }
     })
-
-/* Funcion para sacar estilos de hover*/
-const boton = document.getElementById("miBoton");
-
-setTimeout(function () {
-boton.style.transition = "background-color 0.3s, color 0.3s, transform 0.5s";
-}, 3000);
-
+}
+function cargarCheckboxDescriptores() {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    var imagenContainer = document.getElementById("imagenContainer");
+    var imagenesSeleccionadas = []; 
+    checkboxes.forEach(function (checkbox) {
+      checkbox.addEventListener("change", function () {
+        var descriptor = checkbox.getAttribute("name");
+        if (checkbox.checked) {
+          var imagen = document.createElement("img");
+          imagen.src = `./assets/descriptores-tematicos/descriporestrans/${descriptor}.png`;
+          imagen.width = 360;
+          imagenesSeleccionadas.push({ checkbox, imagen }); // Agregar checkbox e imagen al array
+        } else {
+          // Filtrar el array para eliminar el elemento correspondiente
+          imagenesSeleccionadas = imagenesSeleccionadas.filter(function (item) {
+            return item.checkbox !== checkbox;
+          });
+        }
+        // Actualizar el contenido del contenedor de imágenes
+        imagenContainer.innerHTML = "";
+        imagenesSeleccionadas.forEach(function (item) {
+          imagenContainer.appendChild(item.imagen);
+        });
+      });
+    });
+  }
+  function descargarLogo(){
+    let div = document.querySelector("#logo");
+    let w = div.offsetWidth - 1;
+    let h = div.offsetHeight - 1;
+    html2canvas(div, {
+        border: 0,
+        height: h ,
+        width: w,
+        scale: 5,
+        imageSmoothingEnabled: false
+    })
+    .then((canvas2) => {
+        let a = document.createElement('a');
+        document.body.appendChild(a)
+        a.href = canvas2.toDataURL("image/png");
+        //nombre del archivo con el cual se descarga 
+        let nombreLogo = prompt("Ingrese el nombre del archivo: ");
+        if (!nombreLogo) {
+            // Mostrar un mensaje de error
+            alert("Ingrese el nombre del archivo.");
+            return;
+          }
+        a.download = nombreLogo + '.png';
+        a.click();
+    })
+} 
+document.addEventListener("DOMContentLoaded", function(){
+    cargarSelectCategoriasSinreserva()
+    cargarSelectCategoriasConreserva()
+    cargarSelectLeyendas()
+    cargarCheckboxDescriptores();
+    /* Funcion para sacar estilos de hover*/
+    const boton = document.getElementById("miBoton");
+    setTimeout(function () {
+        boton.style.transition = "background-color 0.3s, color 0.3s, transform 0.5s";
+    }, 3000);
 })
